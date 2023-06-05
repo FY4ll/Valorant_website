92fe57a2e1815321c335ff2dd77ffcb2d473c6e6
FY4ll <fadyallemann@gmail.com>
2023-06-05 09:24:33 +0200

    CHANGED CAROUSEL SYSTEM

--- Web/rank/rank.js
+++ Web/rank/rank.js
@@ -1,105 +1,64 @@
-let rankObj = [
-    "iron",
-    "bronze",
-    "silver",
-    "gold",
-    "platinium",
-    "diamond",
-    "ascendant",
-    "immortal",
-    "radiant"
-]
-document.querySelectorAll(".carousel").forEach((carousel) => {
-    const items = carousel.querySelectorAll(".carousel__item");
-    const buttonsHtml = Array.from(items, () => {
-        return `<span class="carousel__button"></span>`;
-    });
+const carousel = document.getElementById('carousel');
+const slides = carousel.querySelectorAll('.slide');
+let currentSlideIndex = 0; // Index de la deuxième image
+let touchStartX = 0;
+let touchEndX = 0;
 
-    carousel.insertAdjacentHTML(
-        "beforeend",
-        `
-        <div class="carousel__nav">
-            ${buttonsHtml.join("")}
-        </div>
-    `
-    );
+// Le reste du code reste inchangé
+// ...
 
-    const buttons = carousel.querySelectorAll(".carousel__button");
-    let currentItemIndex = 0;
+showSlide(currentSlideIndex);
 
-    buttons.forEach((button, i) => {
-        button.addEventListener("click", () => {
-            setCurrentItem(i);
-        });
-    });
-
-    function setCurrentItem(index) {
-        // un-select all the items
-        items.forEach((item) =>
-            item.classList.remove("carousel__item--selected")
-        );
-        buttons.forEach((button) =>
-            button.classList.remove("carousel__button--selected")
-        );
-
-        items[index].classList.add("carousel__item--selected");
-        buttons[index].classList.add("carousel__button--selected");
-        currentItemIndex = index;
-    }
-
-    // Select the first item on page load
-    setCurrentItem(0);
-
-    // Listen for keyboard events
-    document.addEventListener("keydown", (event) => {
-        switch (event.key) {
-            case "ArrowLeft":
-                if (currentItemIndex > 0) {
-                    setCurrentItem(currentItemIndex - 1);
-                }
-                break;
-            case "ArrowRight":
-                if (currentItemIndex < items.length - 1) {
-                    setCurrentItem(currentItemIndex + 1);
-                }
-                break;
-            default:
-                break;
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.classList.add('active');
+        } else {
+            slide.classList.remove('active');
         }
     });
-
-    // Listen for touch events on mobile devices
-    let touchStartX = 0;
-    let touchEndX = 0;
-
-    carousel.addEventListener("touchstart", (event) => {
-        touchStartX = event.touches[0].clientX;
-
-    });
-
-    carousel.addEventListener("touchend", (event) => {
-        touchEndX = event.changedTouches[0].clientX;
-        handleSwipe();
-    });
-
-    function handleSwipe() {
-        const minSwipeDistance = 50; // Minimum distance required to trigger a swipe
-
-        if (touchEndX - touchStartX > minSwipeDistance && currentItemIndex > 0) {
-            setCurrentItem(currentItemIndex - 1); // Swipe to the left
-        } else if (touchStartX - touchEndX > minSwipeDistance && currentItemIndex < items.length - 1) {
-            setCurrentItem(currentItemIndex + 1); // Swipe to the right
-        }
-    }
-});
-var myImage = document.getElementById(rankObj[0]);
-
-myImage.addEventListener('animationend', function() {
-    myImage.style.animationFillMode = 'forwards';
-});
-var body = document.querySelector('body');
-
-// Fonction pour changer la couleur de l'arrière-plan du corps
-function changeBackgroundColor(color) {
-    body.style.backgroundColor = color;
 }
+
+function nextSlide() {
+    currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+    showSlide(currentSlideIndex);
+}
+
+function prevSlide() {
+    currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
+    showSlide(currentSlideIndex);
+}
+
+function handleTouchStart(event) {
+    touchStartX = event.touches[0].clientX;
+}
+
+function handleTouchEnd(event) {
+    touchEndX = event.changedTouches[0].clientX;
+    handleGesture();
+}
+
+function handleGesture() {
+    const gestureThreshold = 50;
+    if (touchEndX - touchStartX > gestureThreshold) {
+        prevSlide();
+    } else if (touchStartX - touchEndX > gestureThreshold) {
+        nextSlide();
+    }
+}
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'ArrowLeft') {
+        prevSlide();
+    } else if (event.key === 'ArrowRight') {
+        nextSlide();
+    }
+});
+
+carousel.addEventListener('touchstart', handleTouchStart);
+carousel.addEventListener('touchend', handleTouchEnd);
+
+// Désactiver le défilement automatique
+clearInterval(carouselInterval);
+
+showSlide(currentSlideIndex);
